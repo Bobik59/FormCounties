69307a1b107d8eecf19ef890acd113b60d84ba89
Bobik58 <loshininc@gmail.com>
2024-11-03 04:07:32 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxTasks = new System.Windows.Forms.ComboBox();
+            this.dataGridViewResults = new System.Windows.Forms.DataGridView();
+            this.textBoxCountryName = new System.Windows.Forms.TextBox();
+            this.textBoxStartingLetter = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkId = new System.Windows.Forms.CheckBox();
+            this.chkCountryName = new System.Windows.Forms.CheckBox();
+            this.chkPopulation = new System.Windows.Forms.CheckBox();
+            this.chkArea = new System.Windows.Forms.CheckBox();
+            this.chkContinent = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxTasks
+            // 
+            this.comboBoxTasks.FormattingEnabled = true;
+            this.comboBoxTasks.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxTasks.Name = "comboBoxTasks";
+            this.comboBoxTasks.Size = new System.Drawing.Size(389, 21);
+            this.comboBoxTasks.TabIndex = 0;
+            // 
+            // dataGridViewResults
+            // 
+            this.dataGridViewResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResults.Location = new System.Drawing.Point(12, 130);
+            this.dataGridViewResults.Name = "dataGridViewResults";
+            this.dataGridViewResults.Size = new System.Drawing.Size(776, 308);
+            this.dataGridViewResults.TabIndex = 1;
+            // 
+            // textBoxCountryName
+            // 
+            this.textBoxCountryName.Location = new System.Drawing.Point(12, 39);
+            this.textBoxCountryName.Name = "textBoxCountryName";
+            this.textBoxCountryName.Size = new System.Drawing.Size(389, 20);
+            this.textBoxCountryName.TabIndex = 2;
+            // 
+            // textBoxStartingLetter
+            // 
+            this.textBoxStartingLetter.Location = new System.Drawing.Point(407, 39);
+            this.textBoxStartingLetter.Name = "textBoxStartingLetter";
+            this.textBoxStartingLetter.Size = new System.Drawing.Size(389, 20);
+            this.textBoxStartingLetter.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(389, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonExecute_Click);
+            // 
+            // chkId
+            // 
+            this.chkId.AutoSize = true;
+            this.chkId.Location = new System.Drawing.Point(43, 78);
+            this.chkId.Name = "chkId";
+            this.chkId.Size = new System.Drawing.Size(35, 17);
+            this.chkId.TabIndex = 5;
+            this.chkId.Text = "Id";
+            this.chkId.UseVisualStyleBackColor = true;
+            // 
+            // chkCountryName
+            // 
+            this.chkCountryName.AutoSize = true;
+            this.chkCountryName.Location = new System.Drawing.Point(84, 78);
+            this.chkCountryName.Name = "chkCountryName";
+            this.chkCountryName.Size = new System.Drawing.Size(90, 17);
+            this.chkCountryName.TabIndex = 6;
+            this.chkCountryName.Text = "CountryName";
+            this.chkCountryName.UseVisualStyleBackColor = true;
+            // 
+            // chkPopulation
+            // 
+            this.chkPopulation.AutoSize = true;
+            this.chkPopulation.Location = new System.Drawing.Point(180, 78);
+            this.chkPopulation.Name = "chkPopulation";
+            this.chkPopulation.Size = new System.Drawing.Size(76, 17);
+            this.chkPopulation.TabIndex = 7;
+            this.chkPopulation.Text = "Population";
+            this.chkPopulation.UseVisualStyleBackColor = true;
+            // 
+            // chkArea
+            // 
+            this.chkArea.AutoSize = true;
+            this.chkArea.Location = new System.Drawing.Point(253, 78);
+            this.chkArea.Name = "chkArea";
+            this.chkArea.Size = new System.Drawing.Size(48, 17);
+            this.chkArea.TabIndex = 8;
+            this.chkArea.Text = "Area";
+            this.chkArea.UseVisualStyleBackColor = true;
+            // 
+            // chkContinent
+            // 
+            this.chkContinent.AutoSize = true;
+            this.chkContinent.Location = new System.Drawing.Point(307, 78);
+            this.chkContinent.Name = "chkContinent";
+            this.chkContinent.Size = new System.Drawing.Size(71, 17);
+            this.chkContinent.TabIndex = 9;
+            this.chkContinent.Text = "Continent";
+            this.chkContinent.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(407, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnDisplayData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chkContinent);
+            this.Controls.Add(this.chkArea);
+            this.Controls.Add(this.chkPopulation);
+            this.Controls.Add(this.chkCountryName);
+            this.Controls.Add(this.chkId);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxStartingLetter);
+            this.Controls.Add(this.textBoxCountryName);
+            this.Controls.Add(this.dataGridViewResults);
+            this.Controls.Add(this.comboBoxTasks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxTasks;
+        private System.Windows.Forms.DataGridView dataGridViewResults;
+        private System.Windows.Forms.TextBox textBoxCountryName;
+        private System.Windows.Forms.TextBox textBoxStartingLetter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkId;
+        private System.Windows.Forms.CheckBox chkCountryName;
+        private System.Windows.Forms.CheckBox chkPopulation;
+        private System.Windows.Forms.CheckBox chkArea;
+        private System.Windows.Forms.CheckBox chkContinent;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private DataClasses1DataContext context;
+        public Form1()
+        {
+            InitializeComponent();
+            context = new DataClasses1DataContext();
+
+            comboBoxTasks.Items.Add("Показать полную информацию о странах");
+            comboBoxTasks.Items.Add("Показать информацию о конкретной стране");
+            comboBoxTasks.Items.Add("Показать информацию о городах конкретной страны");
+            comboBoxTasks.Items.Add("Показать все страны, чьё имя начинается с буквы");
+            comboBoxTasks.Items.Add("Показать все столицы, чьё имя начинается с буквы");
+            comboBoxTasks.Items.Add("Топ-3 столиц с наименьшим количеством жителей");
+            comboBoxTasks.Items.Add("Топ-3 стран с наименьшим количеством жителей");
+            comboBoxTasks.Items.Add("Среднее количество жителей в столицах по каждой части света");
+            comboBoxTasks.Items.Add("Топ-3 стран по каждой части света с наименьшим количеством жителей");
+            comboBoxTasks.Items.Add("Топ-3 стран по каждой части света с наибольшим количеством жителей");
+            comboBoxTasks.Items.Add("Среднее количество жителей в конкретной стране");
+            comboBoxTasks.Items.Add("Город с наименьшим количеством жителей в конкретной стране");
+        }
+
+        private void btnDisplayData_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            if (chkId.Checked)
+                dt.Columns.Add("Id");
+            if (chkCountryName.Checked)
+                dt.Columns.Add("CountryName");
+            if (chkPopulation.Checked)
+                dt.Columns.Add("Population");
+            if (chkArea.Checked)
+                dt.Columns.Add("Area");
+            if (chkContinent.Checked)
+                dt.Columns.Add("Continent");
+
+            var countries = context.Countries.ToList();
+
+            foreach (var country in countries)
+            {
+                DataRow row = dt.NewRow();
+                if (chkId.Checked)
+                    row["Id"] = country.Id;
+                if (chkCountryName.Checked)
+                    row["CountryName"] = country.CountryName;
+                if (chkPopulation.Checked)
+                    row["Population"] = country.Population;
+                if (chkArea.Checked)
+                    row["Area"] = country.Area;
+                if (chkContinent.Checked)
+                    row["Continent"] = country.Continent;
+
+                dt.Rows.Add(row);
+            }
+            dataGridViewResults.DataSource = dt;
+        }
+
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            string selectedTask = comboBoxTasks.SelectedItem?.ToString();
+            DataTable resultTable = null;
+
+            switch (selectedTask)
+            {
+                case "Показать полную информацию о странах":
+                    resultTable = GetAllCountries();
+                    break;
+                case "Показать информацию о конкретной стране":
+                    string countryName = textBoxCountryName.Text;
+                    resultTable = GetCountryInfo(countryName);
+                    break;
+                case "Показать информацию о городах конкретной страны":
+                    string selectedCountry = textBoxCountryName.Text;
+                    resultTable = GetCitiesByCountry(selectedCountry);
+                    break;
+                case "Показать все страны, чьё имя начинается с буквы":
+                    char startingLetter = textBoxStartingLetter.Text.Length > 0 ? textBoxStartingLetter.Text[0] : ' ';
+                    resultTable = GetCountriesStartingWith(startingLetter);
+                    break;
+                case "Показать все столицы, чьё имя начинается с буквы":
+                    char capitalStartingLetter = textBoxStartingLetter.Text.Length > 0 ? textBoxStartingLetter.Text[0] : ' ';
+                    resultTable = GetCapitalsStartingWith(capitalStartingLetter);
+                    break;
+                case "Топ-3 столиц с наименьшим количеством жителей":
+                    resultTable = GetTop3CapitalsByPopulation();
+                    break;
+                case "Топ-3 стран с наименьшим количеством жителей":
+                    resultTable = GetTop3CountriesByPopulation();
+                    break;
+                case "Среднее количество жителей в столицах по каждой части света":
+                    resultTable = GetAveragePopulationCapitalsByContinent();
+                    break;
+                case "Топ-3 стран по каждой части света с наименьшим количеством жителей":
+                    resultTable = GetTop3CountriesByContinentMinPopulation();
+                    break;
+                case "Топ-3 стран по каждой части света с наибольшим количеством жителей":
+                    resultTable = GetTop3CountriesByContinentMaxPopulation();
+                    break;
+                case "Среднее количество жителей в конкретной стране":
+                    string specificCountry = textBoxCountryName.Text;
+                    resultTable = GetAveragePopulationInCountry(specificCountry);
+                    break;
+                case "Город с наименьшим количеством жителей в конкретной стране":
+                    string countryForCity = textBoxCountryName.Text;
+                    resultTable = GetCityWithMinPopulationInCountry(countryForCity);
+                    break;
+                default:
+                    MessageBox.Show("Выберите задачу.");
+                    return;
+            }
+
+            dataGridViewResults.DataSource = resultTable;
+        }
+
+        private DataTable GetAllCountries()
+        {
+            var countries = context.Countries.ToList();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("CountryName");
+            dt.Columns.Add("Population");
+            dt.Columns.Add("Area");
+            dt.Columns.Add("Continent");
+
+            foreach (var country in countries)
+            {
+                dt.Rows.Add(country.Id, country.CountryName, country.Population, country.Area, country.Continent);
+            }
+
+            return dt;
+        }
+
+        private DataTable GetCountryInfo(string countryName)
+        {
+            var country = context.Countries
+          .FirstOrDefault(c => c.CountryName.ToLower() == countryName.ToLower());
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("CountryName");
+            dt.Columns.Add("Population");
+            dt.Columns.Add("Area");
+            dt.Columns.Add("Continent");
+
+            if (country != null)
+            {
+                dt.Rows.Add(country.Id, country.CountryName, country.Population, country.Area, country.Continent);
+            }
+
+            return dt;
+        }
+
+
+        private DataTable GetCitiesByCountry(string countryName)
+        {
+            var country = context.Countries
+                    .FirstOrDefault(c => c.CountryName.ToLower() == countryName.ToLower()); 
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("CityName");
+            dt.Columns.Add("CityPopulation");
+
+            if (country != null)
+            {
+                var cities = context.Cities.Where(c => c.CountryId == country.Id).ToList();
+                foreach (var city in cities)
+                {
+                    dt.Rows.Add(city.Id, city.CityName, city.CityPopulation);
+                }
+            }
+
+            return dt;
+        }
+
+        private DataTable GetCountriesStartingWith(char letter)
+        {
+            string lowerLetter = letter.ToString().ToLower(); 
+            var countries = context.Countries
+                .Where(c => c.CountryName.ToLower().StartsWith(lowerLetter))
+                .ToList();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("CountryName");
+            dt.Columns.Add("Population");
+            dt.Columns.Add("Area");
+            dt.Columns.Add("Continent");
+
+            foreach (var country in countries)
+            {
+                dt.Rows.Add(country.Id, country.CountryName, country.Population, country.Area, country.Continent);
+            }
+
+            return dt;
+        }
+
+        private DataTable GetCapitalsStartingWith(char letter)
+        {
+            string lowerLetter = letter.ToString().ToLower();
+            var capitals = context.Capitals
+                .Where(c => c.CapitalName.ToLower().StartsWith(lowerLetter)) 
+                .ToList();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("CapitalName");
+            dt.Columns.Add("CapitalPopulation");
+
+            foreach (var capital in capitals)
+            {
+                dt.Rows.Add(capital.Id, capital.CapitalName, capital.CapitalPopulation);
+            }
+
+            return dt;
+        }
+
+        private DataTable GetTop3CapitalsByPopulation()
+        {
+            var capitals = (from c in context.Capitals
+                            orderby c.CapitalPopulation ascending
+                            select new { c.Id, c.CapitalName, c.CapitalPopulation })
+                           .Take(3)
+                           .ToList();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("CapitalName");
+            dt.Columns.Add("CapitalPopulation");
+
+            foreach (var capital in capitals)
+            {
+                dt.Rows.Add(capital.Id, capital.CapitalName, capital.CapitalPopulation);
+            }
+
+            return dt;
+        }
+
+        private DataTable GetTop3CountriesByPopulation()
+        {
+            var countries = (from c in context.Countries
+                             orderby c.Population ascending
+                             select new { c.Id, c.CountryName, c.Population })
+                            .Take(3)
+                            .ToList();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("CountryName");
+            dt.Columns.Add("Population");
+
+            foreach (var country in countries)
+            {
+                dt.Rows.Add(country.Id, country.CountryName, country.Population);
+            }
+
+            return dt;
+        }
+
+        private DataTable GetAveragePopulationCapitalsByContinent()
+        {
+            var averages = from c in context.Capitals
+                           group c by c.Countries.Continent into g
+                           select new
+                           {
+                               Continent = g.Key,
+                               AveragePopulation = g.Average(c => (double?)c.CapitalPopulation) ?? 0
+                           };
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Continent");
+            dt.Columns.Add("AveragePopulation");
+
+            foreach (var average in averages)
+            {
+                dt.Rows.Add(average.Continent, average.AveragePopulation);
+            }
+
+            return dt;
+        }
+
+        private DataTable GetTop3CountriesByContinentMinPopulation()
+        {
+            var topCountries = from c in context.Countries
+                               group c by c.Continent into g
+                               select new
+                               {
+                                   Continent = g.Key,
+                                   Countries = g.OrderBy(c => c.Population).Take(3)
+                               };
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Continent");
+            dt.Columns.Add("CountryName");
+            dt.Columns.Add("Population");
+
+            foreach (var group in topCountries)
+            {
+                foreach (var country in group.Countries)
+                {
+                    dt.Rows.Add(group.Continent, country.CountryName, country.Population);
+                }
+            }
+
+            return dt;
+        }
+
+        private DataTable GetTop3CountriesByContinentMaxPopulation()
+        {
+            var topCountries = from c in context.Countries
+                               group c by c.Continent into g
+                               select new
+                               {
+                                   Continent = g.Key,
+                                   Countries = g.OrderByDescending(c => c.Population).Take(3)
+                               };
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Continent");
+            dt.Columns.Add("CountryName");
+            dt.Columns.Add("Population");
+
+            foreach (var group in topCountries)
+            {
+                foreach (var country in group.Countries)
+                {
+                    dt.Rows.Add(group.Continent, country.CountryName, country.Population);
+                }
+            }
+
+            return dt;
+        }
+
+        private DataTable GetAveragePopulationInCountry(string countryName)
+        {
+            var country = context.Countries
+                    .FirstOrDefault(c => c.CountryName.ToLower() == countryName.ToLower()); 
+            DataTable dt = new DataTable();
+            dt.Columns.Add("AveragePopulation");
+
+            if (country != null)
+            {
+                double averagePopulation = context.Cities
+                    .Where(c => c.CountryId == country.Id)
+                    .Average(c => (double?)c.CityPopulation) ?? 0;
+                dt.Rows.Add(averagePopulation);
+            }
+
+            return dt;
+        }
+
+
+        private DataTable GetCityWithMinPopulationInCountry(string countryName)
+        {
+            var country = context.Countries
+                   .FirstOrDefault(c => c.CountryName.ToLower() == countryName.ToLower());
+            DataTable dt = new DataTable();
+            dt.Columns.Add("CityName");
+            dt.Columns.Add("CityPopulation");
+
+            if (country != null)
+            {
+                var city = context.Cities
+                    .Where(c => c.CountryId == country.Id)
+                    .OrderBy(c => c.CityPopulation)
+                    .FirstOrDefault();
+
+                if (city != null)
+                {
+                    dt.Rows.Add(city.CityName, city.CityPopulation);
+                }
+            }
+
+            return dt;
+        }
+    }
+}
